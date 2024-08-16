e739c69c95f52a75f94c2c4de4addb7cb2326da9
Thomas Platts <67462990+TPlatts04@users.noreply.github.com>
2024-08-16 18:47:27 +0100

    Write data to csv, read data to list, prevent data loss

--- banking_input.py
+++ banking_input.py
@@ -23,10 +23,11 @@ FILENAME = "bank_data.csv"
 CSV_FIELDNAMES = []
 
 accounts = []
+tempList = []
 
 def main():
     print("\nWelcome to Tom's Banking Application!")
-    options = int(input("Would you like to\n(1) Create an account\n(2) Deposit to the account\n(3) Withdraw from the account\n(4) Transfer to another account\nEnter option number here: "))
+    options = int(input("Would you like to:\n(1) Create an account\n(2) Deposit to the account\n(3) Withdraw from the account\n(4) Transfer to another account\nEnter option number here: "))
     match options:
         case 1:
             createAccount()
@@ -39,6 +40,20 @@ def main():
         case "_":
             print("Invalid option, please try again...")
             main()
+
+def appendCSV():
+    with open(FILENAME, "r") as file:
+        reader = csv.DictReader(file, fieldnames=CSV_FIELDNAMES)
+        loopCounter = 0
+        for row in reader:
+            if row["account_name"] != "account_name":
+                accountHolder = BankAccount(row["account_name"], float(row["balance"]))
+                accounts.append(accountHolder)
+            else:
+                loopCounter += 1
+                if loopCounter == len(row):
+                    main()
+        main()
 
 def assignFieldnames():
     with open(FILENAME, "r") as file:
@@ -54,32 +69,57 @@ def createAccount():
     accountInitialDeposit = int(input("\nHow much would you like to initally deposit into the account? £"))
     accountHolder = BankAccount(accountName, accountInitialDeposit)
     accounts.append(accountHolder)
-    print(accounts)
+    with open(FILENAME, "a") as file:
+        file.writelines(f"{accountName},{accountInitialDeposit}")
     main()
 
 def depositAccount():
+    loopCounter = 0
     depositLocation = input("\nWhat account would you like to send this deposit to?\n(Enter name of account holder): ")
     for names in accounts:
         if names.name == depositLocation:
             depositAmount = float(input(f"\nHow much would you like to deposit in {depositLocation}'s account? £"))
             names.deposit(depositAmount)
             names.getBalance()
+            with open(FILENAME, "r") as file:
+                lines = file.readlines()
+                indexOfLine = 0
+                for x in range(len(lines)):
+                    if depositLocation in lines[x].strip().replace(",", " "):
+                        indexOfLine = x
+                lines[indexOfLine] = f"{names.name},{names.balance}\n"
+            with open(FILENAME, "w") as file:
+                file.writelines(lines)
             main()
         else:
-            print("Invalid name, try again")
-            depositAccount()
+            loopCounter += 1
+            if loopCounter == len(accounts):
+                print("Invalid name, try again")
+                depositAccount()
         
 
 def withdrawAccount():
+    loopCounter = 0
     withdrawLocation = input("\nWhich account would you like to withdraw from?\n(Enter name of account holder): ")
     for names in accounts:
         if names.name == withdrawLocation:
             withdrawAmount = float(input("\bHow much would you like to withdraw? £"))
             names.withdraw(withdrawAmount)
+            with open(FILENAME, "r") as file:
+                lines = file.readlines()
+                indexOfLine = 0
+                for x in range(len(lines)):
+                    if withdrawLocation in lines[x].strip().replace(",", " "):
+                        indexOfLine = x
+                lines[indexOfLine] = f"{names.name},{names.balance}\n"
+            with open(FILENAME, "w") as file:
+                file.writelines(lines)
             main()
         else:
-            print("Invalid user, try again...")
-            withdrawAccount()
+            loopCounter += 1
+            if loopCounter == len(accounts):
+                print("Invalid user, try again")
+                depositAccount()
 
 def transferAccount():
     transferStart = input("\nWhich account would you like to transfer from?\n(Enter name of account holder): ")
@@ -91,6 +131,19 @@ def transferAccount():
                 if nameEnd.name == transferEnd:
                     transferAmount = float(input(f"\nHow much would you like to transfer to {transferEnd}'s account? £"))
                     nameStart.transfer(transferAmount, nameEnd)
+                    with open(FILENAME, "r") as file:
+                        lines = file.readlines()
+                        indexOfLine = 0
+                        for x in range(len(lines)):
+                            if transferStart in lines[x].strip().replace(",", " "):
+                                indexOfLine = x
+                        lines[indexOfLine] = f"{nameStart.name},{nameStart.balance}\n"
+                        for y in range(len(lines)):
+                            if transferEnd in lines[x].strip().replace(",", " "):
+                                indexOfLine = x
+                        lines[indexOfLine] = f"{nameEnd.name},{nameEnd.balance}\n"
+                    with open(FILENAME, "w") as file:
+                        file.writelines(lines)
                     main()
                 else:
                     loopCounter += 1
@@ -106,10 +159,10 @@ def transferAccount():
 if __name__ == "__main__":
     if os.path.isfile(FILENAME):
         assignFieldnames()
-        main()
+        appendCSV()
     else:
         f = open(FILENAME, "w")
         f.write("account_name,balance\n")
         f.close()
         assignFieldnames()
-        main()
+        appendCSV()
